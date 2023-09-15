003fae4d5a78ec3b7921c0584326272fb53b136f
MuhammadUsamaAwan <underusama@gmail.com>
2023-09-15 18:23:51 +0500

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from 'elysia';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get('/', () => Bun.file('src/index.html'))
+  .post(
+    '/add',
+    ({ body }) => {
+      return `<li hx-delete="/delete" hx-swap="delete">${body.todo}</li>`;
+    },
+    {
+      body: t.Object({
+        todo: t.String(),
+      }),
+    }
+  )
+  .delete('/delete', () => {
+    return;
+  })
+  .listen(3000);
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
